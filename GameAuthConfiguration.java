6f0a1251d550dcfe8d650109baf6981c0b384dc5
Lawrence Arundel <106612545+oNeglect@users.noreply.github.com>
2022-09-23 18:36:19 -0400

    Update GameAuthConfiguration.java

---
+++ GameAuthConfiguration.java
@@ -0,0 +1,9 @@
+package com.dropwizard.GameAuth;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class GameAuthConfiguration extends Configuration {
+}
